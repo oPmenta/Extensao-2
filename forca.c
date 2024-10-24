d8e20a5bc65762eace58c678cdb280519a153060
Thiago Silva <zanegelo@gmail.com>
2024-10-23 22:18:50 -0300

    23/10/24

--- forca.c
+++ forca.c
@@ -119,7 +119,7 @@ int main()
         // seleciona uma palavra aleatória que ainda não foi usada
         do
         {
-            i = 10;
+            i = rand() % 18;
             // verifica se a palavra já foi usada
             jaUsada = 0;
             for (int j = 0; j < totalUsadas; j++)
@@ -357,7 +357,7 @@ int main()
         // {"mario", "corinthians", "uberaba", "coruja", "guitarra", "baleia", "framboesa", "russo", "submarino", "link", "botafogo", "betim", "pinguim", "flauta", "morcego", "groselha", "hindi", "trem"};
         printf("\n\n\n- Curiosidade\n");
         if (tema == 0)
-            printf("\nMario eh um personagem ficticio da franquia e serie de jogos eletronicos Mario da Nintendo, criado pelo desenvolvedor e designer de jogos eletronicos japones Shigeru Miyamoto. \n");
+            printf("\nMario originalmente se chamava Jumpman em seu primeiro jogo, Donkey Kong (1981). Seu nome foi mudado para Mario em homenagem a Mario Segale, o proprietario do armazem que a Nintendo alugava nos EUA. \n");
         if (tema == 1)
             printf("\nAlem de ser o melhor time do mundo, o mascote do clube, o Mosqueteiro, surgiu de uma cronica do jornal A Gazeta Esportiva apos um amistoso contra o Barracas da Argentina. O texto comparava a garra dos jogadores corintianos com Os Tres Mosqueteiros de Alexandre Dumas. \n");
         if (tema == 2)
@@ -379,19 +379,19 @@ int main()
         if (tema == 10)
             printf("\nO Botafogo detem o recorde de titulos consecutivos do Campeonato Carioca, tendo vencido o torneio por sete anos seguidos, de 1968 a 1974. \n");
         if (tema == 11)
-            printf("\n \n");
+            printf("\nA regiao onde hoje fica Betim era uma importante rota de bandeirantes que vinham de Sao Paulo a Pitangui.  \n");
         if (tema == 12)
-            printf("\n \n");
+            printf("\nEmbora sejam otimos nadadores, os pinguins, apesar de serem aves, nao podem voar. Eles vivem principalmente em regioes do hemisferio sul, como a Antartica. \n");
         if (tema == 13)
-            printf("\n \n");
+            printf("\nA flauta eh um dos instrumentos musicais mais antigos ja encontrados. Arqueologos ja encontraram flautas feitas de ossos de animais, casca de arvore e outros materiais. \n");
         if (tema == 14)
-            printf("\n \n");
+            printf("\nOs morcegos sao os unicos mamiferos que conseguem voar de verdade, ao contrario de animais como esquilos-voadores, que apenas planam. \n");
         if (tema == 15)
-            printf("\n \n");
+            printf("\nA groselha pode ser consumida fresca, mas tambem eh comumente usada para fazer geleias, compotas, sucos, xaropes e ate vinhos. \n");
         if (tema == 16)
-            printf("\n \n");
+            printf("\nO hindi eh a terceira lingua mais falada no mundo, com mais de 600 milhoes de falantes, atras apenas do ingles e do mandarim. \n");
         if (tema == 17)
-            printf("\n \n");
+            printf("\nTrens sao considerados um dos meios de transporte mais seguros do mundo. Acidentes ferroviarios sao raros em comparacao com acidentes de carro ou aviao. \n");
 
         printf("\nPressione qualquer tecla para voltar ao MENU. . .");
         getch();
